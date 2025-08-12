60132eb8ee4f3a995cee7167c4e9201f15821f70
jasmine xu <jasminexu0812@gmail.com>
2025-08-11 21:36:53 -0400

    test: removing authenticate token for testing in prod

--- backend/index.js
+++ backend/index.js
@@ -94,21 +94,21 @@ function logReq(req) {
  * JWT middleware
  */
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = (authHeader && authHeader.split(" ")[1]) || req.cookies?.token;
+  // const authHeader = req.headers["authorization"];
+  // const token = (authHeader && authHeader.split(" ")[1]) || req.cookies?.token;
 
-  if (!token) {
-    return res.status(401).json({ error: "Access token required" });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ error: "Access token required" });
+  // }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ error: "Invalid or expired token" });
-    }
+  // jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  //   if (err) {
+  //     return res.status(403).json({ error: "Invalid or expired token" });
+  //   }
 
-    req.user = user;
+  //   req.user = user;
     next();
-  });
+  // });
 };
 
 /**
